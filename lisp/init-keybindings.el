8f82f78620915b81600b25f1543323f80daff620
Gabriel Nagy <gabrielnagy@me.com>
2019-03-24 15:17:23 +0200

    Add some buffer-related defuns and bindings

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -7,6 +7,7 @@
 (define-key global-map (kbd "C-c s") (lambda () (interactive) (ansi-term "zsh")))
 (define-key global-map (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-x p i") 'org-cliplink)
+(global-set-key (kbd "C-x 3") 'split-window-right-and-move-there-dammit)
 (define-key global-map (kbd "C-M-r") 'eval-buffer)
 (define-key global-map (kbd "<f3>") 'previous-error)
 (define-key global-map (kbd "<f4>") 'next-error)
@@ -15,6 +16,7 @@
 (define-key global-map (kbd "C-}") 'air-cycle-theme)
 (define-key global-map (kbd "C-{") 'cycle-powerline-separators)
 (define-key global-map (kbd "M-\\") 'shell-command-on-region)
+(define-key global-map (kbd "C-x t") 'toggle-window-split)
 
 (define-key global-map (kbd "H-g") 'gomoku)
 (define-key global-map (kbd "H-m") 'mines)
--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -1,4 +1,4 @@
-;;; init-utils.el --- Utilities borrowed from Steve Purcell
+;;; init-utils.el --- Various functions and utilities
 ;;; Commentary:
 ;;; Code:
 (if (fboundp 'with-eval-after-load)
@@ -88,7 +88,38 @@
         (error "Cannot open tramp file")
       (browse-url (concat "file://" file-name)))))
 
+;;----------------------------------------------------------------------------
+;; Change window split direction
+;;----------------------------------------------------------------------------
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+	     (next-win-buffer (window-buffer (next-window)))
+	     (this-win-edges (window-edges (selected-window)))
+	     (next-win-edges (window-edges (next-window)))
+	     (this-win-2nd (not (and (<= (car this-win-edges)
+					 (car next-win-edges))
+				     (<= (cadr this-win-edges)
+					 (cadr next-win-edges)))))
+	     (splitter
+	      (if (= (car this-win-edges)
+		     (car (window-edges (next-window))))
+		  'split-window-horizontally
+		'split-window-vertically)))
+	(delete-other-windows)
+	(let ((first-win (selected-window)))
+	  (funcall splitter)
+	  (if this-win-2nd (other-window 1))
+	  (set-window-buffer (selected-window) this-win-buffer)
+	  (set-window-buffer (next-window) next-win-buffer)
+	  (select-window first-win)
+	  (if this-win-2nd (other-window 1))))))
+
+(defun split-window-right-and-move-there-dammit ()
+  (interactive)
+  (split-window-right)
+(windmove-right))
 
 (provide 'init-utils)
 ;;; init-utils.el ends here
-
