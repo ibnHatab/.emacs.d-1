115fdb02e9f0e34203f8dbec1d7acf29f568fd5e
Gabriel Nagy <gabriel.nagy@nokia.com>
2019-04-03 16:10:19 +0300

    Remove commented irony

--- lisp/init-cpp.el
+++ lisp/init-cpp.el
@@ -60,30 +60,6 @@
   (when (f-exists? (expand-file-name ".clang-format" (projectile-project-root)))
     (clang-format-buffer))))
 
-;; (use-package irony
-;;   :ensure t
-;;   :config
-;;   (use-package company-irony
-;;     :ensure t
-;;     :config
-;;     (add-to-list 'company-backends 'company-irony))
-;;   (use-package company-irony-c-headers
-;;     :ensure t
-;;     :config
-;;     (add-to-list 'company-backends 'company-irony-c-headers))
-;;   (add-hook 'c++-mode-hook 'irony-mode)
-;;   (add-hook 'c-mode-hook 'irony-mode)
-;;   (add-hook 'objc-mode-hook 'irony-mode)
-;;   ;; replace the `completion-at-point' and `complete-symbol' bindings in
-;;   ;; irony-mode's buffers by irony-mode's function
-;;   (defun my-irony-mode-hook ()
-;;     (define-key irony-mode-map [remap completion-at-point]
-;;       'irony-completion-at-point-async)
-;;     (define-key irony-mode-map [remap complete-symbol]
-;;       'irony-completion-at-point-async))
-;;   (add-hook 'irony-mode-hook 'my-irony-mode-hook)
-;;   (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
-
 (defun my-c++-mode-hook ()
   (c-set-style "stroustrup")
   (add-hook 'before-save-hook 'clang-format-buffer-smart nil 'local))
