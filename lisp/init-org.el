eb77ca03d94b50ea1576024816a7d549ca6e445d
Gabriel Nagy <gabriel.nagy@nokia.com>
2019-04-03 16:08:58 +0300

    Remove org-brain package

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -20,20 +20,6 @@
   :config
   (add-hook 'org-mode-hook 'org-bullets-mode))
 
-(use-package org-brain :ensure t
-  :init
-  (setq org-brain-path "~/org/brain")
-  ;; For Evil users
-  ;; (eval-after-load 'evil
-  ;;   (evil-set-initial-state 'org-brain-visualize-mode 'emacs))
-  :config
-  (setq org-id-track-globally t)
-  (setq org-id-locations-file "~/.emacs.d/.org-id-locations")
-  ;; (push '("b" "Brain" plain (function org-brain-goto-end)
-  ;;         "* %i%?" :empty-lines 1))
-        ;; org-capture-templates)
-  (setq org-brain-visualize-default-choices 'all)
-  (setq org-brain-title-max-length 12))
 
 (use-package evil-org
   :ensure t
